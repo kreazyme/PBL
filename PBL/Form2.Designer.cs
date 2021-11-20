a763b439e94da763fd8e626e8d0e7d998b8d4220
Kreazy <102190042@sv1.dut.udn.vn>
2021-11-20 23:03:26 +0700

    thich thi commit

--- PBL/Form2.Designer.cs
+++ PBL/Form2.Designer.cs
@@ -28,19 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.CBB_ListHost = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // CBB_ListHost
+            // 
+            this.CBB_ListHost.FormattingEnabled = true;
+            this.CBB_ListHost.Location = new System.Drawing.Point(13, 13);
+            this.CBB_ListHost.Name = "CBB_ListHost";
+            this.CBB_ListHost.Size = new System.Drawing.Size(169, 24);
+            this.CBB_ListHost.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(227, 143);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            this.chart1.Size = new System.Drawing.Size(645, 425);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 509);
+            this.ClientSize = new System.Drawing.Size(985, 618);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.CBB_ListHost);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox CBB_ListHost;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- PBL/Form2.cs
+++ PBL/Form2.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZabbixApi;
 
 namespace PBL
 {
     public partial class Form2 : Form
     {
+        private int hostid;
         public Form2()
         {
             InitializeComponent();
+            Zabbix zabbix = new Zabbix("Admin", "zabbix", "http://192.168.1.6/zabbix/api_jsonrpc.php");
+            zabbix.login();
+            Response responseObj = zabbix.objectResponse("host.get", new
+            {
+                output =new String[] { "hostid", "host"},
+                       selectInterfaces =  new String[] {"interfaceid","ip"},
+            });
+            foreach(dynamic data in responseObj.result)
+            {
+                Host_Item item = new Host_Item();
+                item.Text = data.host;
+                item.Value = data.hostid;
+                CBB_ListHost.Items.Add(item);
+            }
+            hostid = Convert.ToInt32(responseObj.result[0].hostid);
+            CBB_ListHost.SelectedIndex = 0;
+
+
+
+
+
+            responseObj = zabbix.objectResponse("graph.get", new
+            {
+                graphid = 910,
+                hostids =  hostid,
+            });
+            zabbix.logout();
         }
     }
 }
