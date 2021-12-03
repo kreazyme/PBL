b940943c830606074c52b422c9844a5567401f3c
Kreazy <102190042@sv1.dut.udn.vn>
2021-12-03 09:16:01 +0700

    Vẽ được chart rồi

--- PBL/Form2.Designer.cs
+++ PBL/Form2.Designer.cs
@@ -30,41 +30,104 @@
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.CBB_ListHost = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // CBB_ListHost
             // 
+            this.CBB_ListHost.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CBB_ListHost.FormattingEnabled = true;
-            this.CBB_ListHost.Location = new System.Drawing.Point(13, 13);
+            this.CBB_ListHost.Location = new System.Drawing.Point(0, 0);
             this.CBB_ListHost.Name = "CBB_ListHost";
-            this.CBB_ListHost.Size = new System.Drawing.Size(169, 24);
+            this.CBB_ListHost.Size = new System.Drawing.Size(216, 36);
             this.CBB_ListHost.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(133)))), ((int)(((byte)(244)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1128, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin Dashboard";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.CBB_ListHost);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 632);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // chart1
             // 
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Fuchsia;
+            this.chart1.BorderSkin.BackImageTransparentColor = System.Drawing.Color.Teal;
+            this.chart1.BorderSkin.BackSecondaryColor = System.Drawing.Color.Lime;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.IndianRed;
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.BorderColor = System.Drawing.Color.IndianRed;
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "line1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(227, 143);
+            this.chart1.Location = new System.Drawing.Point(216, 100);
             this.chart1.Name = "chart1";
-            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
-            this.chart1.Size = new System.Drawing.Size(645, 425);
-            this.chart1.TabIndex = 1;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.LabelBackColor = System.Drawing.Color.Yellow;
+            series1.LabelBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "line1";
+            series1.Name = "line1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(912, 632);
+            this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            title1.ForeColor = System.Drawing.Color.BlanchedAlmond;
+            title1.Name = "Title1";
+            this.chart1.Titles.Add(title1);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 618);
+            this.ClientSize = new System.Drawing.Size(1128, 732);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.CBB_ListHost);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
@@ -73,6 +136,9 @@
         #endregion
 
         private System.Windows.Forms.ComboBox CBB_ListHost;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- PBL/Form2.cs
+++ PBL/Form2.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,14 +20,14 @@ namespace PBL
         public Form2()
         {
             InitializeComponent();
-            Zabbix zabbix = new Zabbix("Admin", "zabbix", "http://192.168.1.6/zabbix/api_jsonrpc.php");
+            Zabbix zabbix = new Zabbix("Admin", "zabbix", "http://192.168.96.143/zabbix/api_jsonrpc.php");
             zabbix.login();
             Response responseObj = zabbix.objectResponse("host.get", new
             {
-                output =new String[] { "hostid", "host"},
-                       selectInterfaces =  new String[] {"interfaceid","ip"},
+                output = new String[] { "hostid", "host" },
+                selectInterfaces = new String[] { "interfaceid", "ip" },
             });
-            foreach(dynamic data in responseObj.result)
+            foreach (dynamic data in responseObj.result)
             {
                 Host_Item item = new Host_Item();
                 item.Text = data.host;
@@ -33,17 +36,33 @@ namespace PBL
             }
             hostid = Convert.ToInt32(responseObj.result[0].hostid);
             CBB_ListHost.SelectedIndex = 0;
-
-
-
-
-
-            responseObj = zabbix.objectResponse("graph.get", new
+            responseObj = zabbix.objectResponse("history.get", new
             {
-                graphid = 910,
-                hostids =  hostid,
+                hostids = 10460,
+                itemids = 38698,
+                limit = 10,
             });
-            zabbix.logout();
+            int i = 0;
+            chart1.Series["line1"].XValueMember = "Thời gian";
+
+            foreach (dynamic data in responseObj.result)
+            {
+                i++;
+                int y = Convert.ToInt32(data.value);
+                chart1.Series["line1"].Points.AddXY(i, y);
+            }
+            responseObj = zabbix.objectResponse("item.get", new
+            {
+                itemids = 38698,
+            });
+            chart1.Titles.Add(responseObj.result[0].name);
+
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
